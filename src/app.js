87b03cb1e24a1d0cdd946fbe8b77c05bef163f48
Tonimir10 <91009470+Tonimir10@users.noreply.github.com>
2025-04-17 17:47:32 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+const quien = '¿Quien es culpable?';
+const accion = '¿Que hizo?';
+const que = '¿Que quieres evitar?';
+const cuando = '¿Cuando?';
+const randomWho = Math.floor(Math.random()*who.length);
+const randomAction = Math.floor(Math.random()*action.length);
+const randomWhat = Math.floor(Math.random()*what.length);
+const randomWhen = Math.floor(Math.random()*when.length);
+
+document.getElementById("excuse").innerHTML = who[randomWho] + " " + action[randomAction] + " " + what[randomWhat] + " " + when[randomWhen] ;
+document.getElementById("preguntas").innerHTML = quien +" "+ accion + " " + que + " " + cuando ;
 };
